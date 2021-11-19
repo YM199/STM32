d4853c9a2582c4149629dd5de9c4c912e9d0e14e
Yang Mou <mr_yang_mou@163.com>
2021-11-19 23:13:27 +0800

    update

--- HAL library/LCD/Core/Src/fsmc.c
+++ HAL library/LCD/Core/Src/fsmc.c
@@ -58,10 +58,10 @@ void MX_FSMC_Init(void)
   hsram1.Init.AsynchronousWait = FSMC_ASYNCHRONOUS_WAIT_DISABLE;
   hsram1.Init.WriteBurst = FSMC_WRITE_BURST_DISABLE;
   /* Timing */
-  Timing.AddressSetupTime = 15;
+  Timing.AddressSetupTime = 1;
   Timing.AddressHoldTime = 15;
-  Timing.DataSetupTime = 255;
-  Timing.BusTurnAroundDuration = 15;
+  Timing.DataSetupTime = 4;
+  Timing.BusTurnAroundDuration = 0;
   Timing.CLKDivision = 16;
   Timing.DataLatency = 17;
   Timing.AccessMode = FSMC_ACCESS_MODE_A;
---
+++ HAL library/LCD/README.md
@@ -0,0 +1,9 @@
+# 注意事项：
+
+```C
+  Timing.AddressSetupTime = 1;
+  Timing.AddressHoldTime = 15;
+  Timing.DataSetupTime = 4;
+```
+
+这三个参数即使不按照手册来也能正常运行，不知道为什么。
